b190b71ab4395e0de0b7725b499774a9e3c2ccd7
Steven Leonardo Garzon Ospina <88815263+zPopeyex@users.noreply.github.com>
2025-08-07 15:43:39 -0500

    feat: add user management modal and spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../index.css";
+
+const Spinner = () => (
+  <div className="spinner" role="status" aria-label="loading">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default Spinner;
