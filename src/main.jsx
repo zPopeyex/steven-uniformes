0a789181e121d37e30a4c347405781e90d1b713c
Steven Leonardo Garzon Ospina <88815263+zPopeyex@users.noreply.github.com>
2025-08-07 16:03:54 -0500

    Add loading context provider

--- src/main.jsx
+++ src/main.jsx
@@ -7,23 +7,26 @@ import App from './App.jsx'
 import Login from './pages/Login.jsx'
 import PrivateRoute from './components/PrivateRoute.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { LoadingProvider } from './context/LoadingContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/*"
-            element={
-              <PrivateRoute>
-                <App />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </AuthProvider>
+      <LoadingProvider>
+        <AuthProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/*"
+              element={
+                <PrivateRoute>
+                  <App />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </AuthProvider>
+      </LoadingProvider>
     </BrowserRouter>
   </StrictMode>,
 )
